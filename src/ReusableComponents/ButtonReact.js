a75347766cb21f839ac2ba7fc3d3a99b9f50b00e
Fatima Khalid <69491300+fatimakhalid61@users.noreply.github.com>
2021-07-15 15:20:55 +0500

    Delete src directory

--- src/ReusableComponents/ButtonReact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function ButtonReact({ children, ...otherProps}) {
-  return<button style={{width:'10rem', height:'3rem'}} {...otherProps}>
-  {children}
-</button> ;
-}
-
-export default ButtonReact;
--- src/ReusableComponents/Section.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Section({children}) {
-  
-  return <div style={{fontFamily: "Mulish, sans-serif"}}  class="w-screen h-auto p-10 grid grid-flow-col gap-22 clear-both bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      {children}</div>;
-}
-
-export default Section;
